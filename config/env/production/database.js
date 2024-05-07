2ba6018e28cd884940f6830d6c9d472d887f9724
Mohamed Nabil <medosara2011@gmail.com>
2024-05-07 14:34:17 +0300

    create pg database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,37 @@
-// path: ./config/env/production/database.js
+// // path: ./config/env/production/database.js
 
-const { parse } = require("pg-connection-string");
+// // const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+// module.exports = ({ env }) => {
+//   // const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
-  return {
+//   return {
+//     connection: {
+//       client: "postgres",
+//       connection: {
+//         host:env(''),
+//         port,
+//         database,
+//         user,
+//         password,
+//         ssl: { rejectUnauthorized: false },
+//       },
+//       debug: false,
+//     },
+//   };
+// };
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
-      },
-      debug: false,
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "strapi"),
+      user: env("PGUSER", "strapi"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
     },
-  };
-};
+    pool: { min: 0 },
+  },
+});
